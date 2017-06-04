61088bbfd751d0fb4dc4ff499b0f3cf1b32044d7
spdskatr <tunan.shi@gmail.com>
2017-06-04 21:44:44 +1000

    0.17.1.1
    Updated tech level for repeatable to "Spacer", because of problems with GHXX's Tech Advancing

---
+++ Source/ResearchableStatUpgrades/WorldComponent_DefEditingResearchManager.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace ResearchableStatUpgrades
+{
+    public interface IUndoable
+    {
+        void Apply();
+        void Undo(object val);
+    }
+    public class GameComponent_DefEditingResearchManager : GameComponent
+    {
+        public GameComponent_DefEditingResearchManager() { }
+        public GameComponent_DefEditingResearchManager(Game game) { }
+        public Dictionary<IUndoable, object> undoables;
+        public void ApplyAndRecord(Action a, IUndoable instance, object oldVal)
+        {
+            //Cache old value
+            undoables.Add(instance, oldVal);
+            //Execute
+            a();
+        }
+    }
+}
